6b87a03db9936f86437f35c3fd5075fc2f2681f3
volodymyr <vvavodymyr@gmail.com>
2025-02-16 14:11:05 +0100

    теперь игра запускается как из командной сторки так и через иконку приложения

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-java --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml \
--Dprism.verbose=true \
+export _JAVA_OPTIONS='-Dsun.java2d.opengl=true'
+
+# Переход в папку проекта
+cd /home/volodymyr/IdeaProjects/com.javarush.vasiliev_v.maven
+
+# Запуск игры
+exec java --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml \
+-Dprism.order=sw -Dprism.verbose=true -Dprism.maxvram=512M \
 -Djava.library.path=/usr/lib/x86_64-linux-gnu/jni/ \
--jar target/project-maven-1.0-jar-with-dependencies.jar
+-jar /home/volodymyr/IdeaProjects/com.javarush.vasiliev_v.maven/target/project-maven-1.0-jar-with-dependencies.jar >> ~/racer-game.log 2>&1
